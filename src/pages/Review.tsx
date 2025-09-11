cd2216ae86ae42c1752e7c158ace23002c00d41a
sandiput <sandipratama207@gmail.com>
2025-09-11 15:42:58 +0700

    menyelesaikan menu review

--- src/pages/Review.tsx
+++ src/pages/Review.tsx
@@ -526,7 +526,7 @@ export const Review: React.FC = () => {
         </div>
 
         {/* Quick Actions */}
-        <div className="bg-white rounded-lg p-6 shadow-sm border">
+        {/* <div className="bg-white rounded-lg p-6 shadow-sm border">
           <div className="flex items-center gap-3 mb-6">
             <div className="p-2 rounded-lg bg-indigo-100">
               <FileText className="w-5 h-5 text-indigo-600" />
@@ -565,7 +565,7 @@ export const Review: React.FC = () => {
               </div>
             </button>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
